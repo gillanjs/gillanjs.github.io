6eacbdde59312c09420f235824d1ef1652bfc59e
Gillan <gillanjs@live.co.uk>
2017-06-29 10:47:25 +0100

    Continued editing gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
 
+/*
 gulp.task('log', function() {
   gutil.log('== My Log Task ==')
 });
+*/
+
+
+gulp.task('js', function() {
+  gulp.src('scripts/*.js')
+  .pipe(uglify())
+  .pipe(concat('script.js'))
+  .pipe(gulp.dest('assets'))
+});
+
+gulp.task('default', ['js']);
