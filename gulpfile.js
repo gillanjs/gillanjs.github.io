302cac7b3e9de50747125da11626896c863a2794
Gillan <gillanjs@live.co.uk>
2017-06-29 11:47:08 +0100

    Completed gulp file, updated package.json with CleanCSS, cleared index.html, created & cleared style.css

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,40 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var uglify = require('gulp-uglify'),
     concat = require('gulp-concat');
-
-/*
-gulp.task('log', function() {
-  gutil.log('== My Log Task ==')
-});
-*/
-
+var connect = require('gulp-connect');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('js', function() {
   gulp.src('scripts/*.js')
   .pipe(uglify())
   .pipe(concat('script.js'))
   .pipe(gulp.dest('assets'))
+  .pipe(connect.reload())
 });
 
-gulp.task('default', ['js']);
+gulp.task('css', function() {
+  gulp.src('styles/*.css')
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(gulp.dest('assets'))
+  .pipe(connect.reload())
+});
+
+gulp.task('default', ['html', 'js', 'connect', 'watch']);
+
+gulp.task('watch', function() {
+  gulp.watch('scripts/*.js', ['js']);
+  gulp.watch('styles/*.css', ['css']);
+  gulp.watch('**/*.html', ['html']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: '.',
+    livereload: true
+  })
+});
+
+gulp.task('html', function() {
+  gulp.src('**/*.html')
+  .pipe(connect.reload())
+});
